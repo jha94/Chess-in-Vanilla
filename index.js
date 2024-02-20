f87c29a648360439d75e99fe623649e6288731cc
pjha7 <prashant_jha1@intuit.com>
2024-02-20 19:31:38 +0530

    Chess in VanillaJS

---
+++ index.js
@@ -0,0 +1,7 @@
+const board = document.getElementById('board');
+const evenBox = '<div id="evenBox"></div>';
+const oddBox = '<div id="oddBox"></div>';
+for(let index=0;index<4;index++){
+    board.innerHTML+=evenBox.repeat(8);
+    board.innerHTML+=oddBox.repeat(8);
+}
